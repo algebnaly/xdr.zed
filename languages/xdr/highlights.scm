36b373bdd0b1bc814678fbb5ca3e671d84707e4d
yalikes <algebnaly@qq.com>
2025-03-23 14:09:39 +0800

    init commit

---
+++ languages/xdr/highlights.scm
@@ -0,0 +1,21 @@
+(block_comment) @comment
+
+(keyword_enum) @keyword
+(keyword_struct) @keyword
+(keyword_const) @keyword
+(keyword_union) @keyword
+(keyword_default) @keyword
+(keyword_case) @keyword
+(keyword_switch) @keyword
+(keyword_typedef) @keyword
+
+(keyword_string) @type
+(keyword_opaque) @type
+
+(constant) @number
+(type_specifier) @type
+
+(enum_body (identifier) @variant)
+(type_def (identifier) @type)
+
+(constant_def (identifier) @constant)
